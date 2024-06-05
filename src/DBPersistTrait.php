9079808e97391c67aa130360e92d91e8ca0ac1ce
Johannes <theking2@king.ma>
2024-06-05 14:39:42 +0200

    call back for pk Fixes #41

--- src/DBPersistTrait.php
+++ src/DBPersistTrait.php
@@ -282,6 +282,10 @@ trait DBPersistTrait
 	protected function insert(): bool
 	{
 		try {
+			if( $this->isPrimaryKeyAutoIncrement() ) {
+				$this->{$this->getPrimaryKey()} = $this->nextPrimaryKey();
+			}
+
 			if( $this->getInsertStatement()->execute() ) {
 				if( $this->isPrimaryKeyAutoIncrement() ) {
 					$this->{$this->getPrimaryKey()} = (int) Database::getConnection()->lastInsertId();
@@ -354,11 +358,11 @@ trait DBPersistTrait
 	/* #region Traversal */
 
 	/**
-			* find – find records in the \Kingsoft\Db\Database
+			 * find – find records in the \Kingsoft\Db\Database
 
-			* @throws \Kingsoft\Db\DatabaseException
-			* @return null|object
-			*/
+			 * @throws \Kingsoft\Db\DatabaseException
+			 * @return null|object
+			 */
 	public static function find( ?array $where = [], ?array $order = [] ): null|static
 	{
 		$obj = new static( where: $where, order: $order );
