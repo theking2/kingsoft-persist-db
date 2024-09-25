3ceb0ddec7c56543841db658f2824fb13eec20e1
theking2 <theking2@king.ma>
2024-09-25 15:49:03 +0200

    Handle SET types Fixes #53

--- discover.php
+++ discover.php
@@ -22,12 +22,13 @@ use \Kingsoft\Utils\Format as Format;
  * Map SQL domains to php types
  */
 $type_list = [ 
-  'int' => [ 'int', 'integer', 'mediumint', 'smallint', 'tinyint', 'bigint' ],
-  'float' => [ 'float', 'double', 'real' ],
-  'string' => [ 'char', 'varchar', 'text', 'tinytext', 'mediumtext', 'longtext', 'enum', 'set' ],
-  'bool' => [ 'bool', 'boolean' ],
-  'Date' => [ 'date' ],
+  'int'       => [ 'int', 'integer', 'mediumint', 'smallint', 'tinyint', 'bigint' ],
+  'float'     => [ 'float', 'double', 'real' ],
+  'string'    => [ 'char', 'varchar', 'text', 'tinytext', 'mediumtext', 'longtext', 'enum' ],
+  'bool'      => [ 'bool', 'boolean' ],
+  'Date'      => [ 'date' ],
   '\DateTime' => [ 'datetime' ],
+  'set'       => [ 'set' ]
 ];
 
 $db = \Kingsoft\Db\Database::getConnection();
@@ -67,6 +68,7 @@ function doTable( $table_name )
   $cols = [];
 
   $type_pattern = '/(\w*)(\((\d*)\))?(\s(\w*))?/';
+  $hasSet       = false;
   while( $cols_stat->fetch() ) {
     if( $fieldKey === 'PRI' ) {
       $keyname          = $fieldName;
@@ -74,12 +76,19 @@ function doTable( $table_name )
     }
     preg_match( $type_pattern, $fieldType, $desc );
     foreach( $type_list as $php_type => $db_types ) {
-      if( in_array( $desc[1], $db_types ) ) {
-        $cols[ $fieldName ] = [ $php_type, $desc[3] ?? 0, $desc[5] ?? '' ];
+      if( in_array( $desc[ 1 ], $db_types ) ) {
+        $cols[ $fieldName ] = [ $php_type, $desc[ 3 ] ?? 0, $desc[ 5 ] ?? '' ];
+        if( $php_type === 'set' ) {
+          $hasSet                  = true;
+          $cols[ $fieldName ][ 2 ] = explode( ",", mb_substr( $fieldType, 4, -1 ) ); // remove 'set(' and ')'
+        }
+
         break;
       }
     }
+    $cols[ $fieldName ][ 3 ] = $fieldType;
   }
+
   echo Html::wrap_tag( 'p', "key: " . ( $keyname ?? 'none' ) );
   echo '<ul>';
   foreach( $cols as $fieldName => $fieldDescription ) {
@@ -90,18 +99,7 @@ function doTable( $table_name )
   //$cols = "'" . implode( "',\n\t\t\t'", $cols ) . "'";
   fwrite( $fh, "<?php declare(strict_types=1);\n" );
   fprintf( $fh, "namespace %s;\n\n", _NAMESPACE );
-  fprintf( $fh, "/**\n * %s – Persistant DB object\n", $table_name );
-
-  // Set the datatype for Date and DateTime to PHP \DateTime
-  foreach( $cols as $fieldName => $fieldDescription ) {
-    fprintf( $fh, " * %-10s\$%s;\n",
-      $fieldDescription[0] === 'Date'
-      ? '\DateTime'
-      : $fieldDescription[0],
-      $fieldName
-    );
-  }
-  ;
+  fprintf( $fh, "/**\n * Persistant DB object for table – %s\n", $table_name );
 
   fwrite( $fh, " */\n" );
 
@@ -109,28 +107,41 @@ function doTable( $table_name )
   fwrite( $fh, "\timplements \\Kingsoft\\Persist\\IPersist\n{\n", );
   fwrite( $fh, "\tuse \\Kingsoft\Persist\Db\\DBPersistTrait;\n\n" );
 
+  // create the set constants
+  if( $hasSet ) {
+    foreach( $cols as $fieldName => $fieldDescription ) {
+      if( $fieldDescription[ 0 ] === 'set' ) {
+        $bit = 1;
+        foreach( $fieldDescription[ 2 ] as $set_value ) {
+          fprintf( $fh, "\tconst %s_%s = 0x%x;\n", $fieldName, str_replace( [ "'", " " ], [ "", "_" ], $set_value ), $bit );
+          $bit <<= 1;
+        }
+      }
+    }
+  }
   // Set the datatype for Date and DateTime to PHP \DateTime
   foreach( $cols as $fieldName => $fieldDescription ) {
     fprintf( $fh, "\tprotected ?%-10s\$%s;\n",
-      $fieldDescription[0] === 'Date'
-      ? '\DateTime'
-      : $fieldDescription[0],
+      match ( $fieldDescription[ 0 ] ) {
+        'Date'  => '\DateTime',
+        'set'   => 'int',
+        default => $fieldDescription[ 0 ],
+      },
       $fieldName
     );
   }
-  ;
 
   fwrite( $fh, "\n\t// Persist functions\n" );
   if( isset( $keyname ) ) {
     fprintf( $fh, "\tstatic public function getPrimaryKey():string { return '%s'; }\n", $keyname );
     fprintf( $fh, "\tstatic public function isPrimaryKeyAutoIncrement():bool { return %s; }\n", $hasAutoIncrement ? 'true' : 'false' );
     if( !$hasAutoIncrement ) {
-      switch( $cols[ $keyname ][0] ) {
+      switch( $cols[ $keyname ][ 0 ] ) {
         case 'int':
           fprintf( $fh, "\tstatic public function nextPrimaryKey():int { return 0; }\n" );
           break;
         case 'string':
-          fprintf( $fh, "\tstatic public function nextPrimaryKey():string { return \"%s-\" . bin2hex(random_bytes(12)); }\n", $table_name);
+          fprintf( $fh, "\tstatic public function nextPrimaryKey():string { return \"%s-\" . bin2hex(random_bytes(12)); }\n", $table_name );
           break;
         default:
           fprintf( $fh, "\t//static public function nextPrimaryKey():string { return ''; }\n" );
@@ -145,7 +156,7 @@ function doTable( $table_name )
   fwrite( $fh, "\tstatic public function getFields():array {\n" );
   fwrite( $fh, "\t\treturn [\n" );
   foreach( $cols as $fieldName => $fieldDescription ) {
-    fprintf( $fh, "\t\t\t%-20s => ['%s', %d ],\n", "'$fieldName'", $fieldDescription[0], $fieldDescription[1] );
+    fprintf( $fh, "\t\t\t%-20s => ['%s', %d ], \t\t//\t%s\n", "'$fieldName'", $fieldDescription[ 0 ], $fieldDescription[ 1 ], $fieldDescription[ 3 ] );
   }
   ;
   fwrite( $fh, "\t\t];\n\t}\n}" );
